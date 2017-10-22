cb9bf8e9d702b1f89f9181a65007db1723ad4604
AmberFu <spashleyfu@gmail.com>
2017-10-22 16:51:25 +0800

    Done

--- ptt_all_post_v2.py
+++ ptt_all_post_v2.py
@@ -1,66 +1,189 @@
-import sys
+import datetime
 import getopt
+import sys
 from termcolor import colored
-import time
-
-# get max pages number from index page:
-def get_max_pageNumber():
-    import requests as rs
-    from bs4 import BeautifulSoup
-    import time
-    import pandas as pd
-
-    url = 'https://www.ptt.cc/bbs/ALLPOST/index.html'
-    res = rs.get(url)
-    if res.status_code == 200:
-        # get page text
-        content = res.text
-        soup = BeautifulSoup(content, "html.parser")
-        # get next page :
-        next_page = soup.find_all('a', 'btn wide')[1]['href']
-        pageNum = next_page.split('/', 3)[3].split('.')[0]
-        Num = int(pageNum[5:]) + 1
-        # get date :
-        currentDate = time.strftime("%Y-%m-%d", time.localtime())
-        rent = soup.find_all('div', 'r-ent')
-        date_lists = []
-        for md in rent:
-            date_lists.append(md.find('div', 'date').string)
-        maxDate = date_lists[-1]
-        minDate = date_lists[0]
-        print('(max, min) = (', maxDate, ', ', minDate, ')')
-        print('currentDate = ', currentDate)
-        print('date_lists = ', date_lists)
-        return Num
-    else:
-        print(colored('status_code != 200', 'red'))
 
 
 # get content from url:
-def get_content(url):
+def get_contentSoup(url):
     import requests
-    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    import urllib3
+    from bs4 import BeautifulSoup
     from_str = url.split('/', 3)[3]
-
+    # For over18 POST:
     load = {
         'from': from_str,
         'yes': 'yes'
     }
+    # For GET:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0',
+        'Host': 'www.ptt.cc',
+        'Connection': 'keep-alive',
+    }
+
     rs = requests.session()
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    urllib3.disable_warnings()
     res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=load)
-    res = rs.get(url)
+    res = rs.get(url, headers=headers)
     if res.status_code == 200:
         # get page text
         content = res.text
-        return content
+        soup = BeautifulSoup(content, "html.parser")
+        return soup
     else:
         print(colored('status_code != 200', 'red'))
+
+# get max pages number from index page:
+def get_total_pageNumber(soup):
+    # get next page :
+    next_page = soup.find_all('a', 'btn wide')[1]['href']
+    pageNum = next_page.split('/', 3)[3].split('.')[0]
+    Num = int(pageNum[5:]) + 1
+    return Num
+
+
+
+# get max_date and min_date: the newest article is at end of list.
+def get_max_min_date(soup):
+    import datetime
+    # get date :
+    rent = soup.find_all('div', 'r-ent')
+    date_lists = []
+    for md in rent:
+        date_lists.append(md.find('div', 'date').string)
+
+    today_date = datetime.date.today()
+    this_year = today_date.year
+
+    # put today.year as year: if max = 2018/01/02, min = 2017/12/30 ~> 2018/12/30, today = 2018/01/05
+    max_date = '{}/{}'.format(this_year, date_lists[-1].strip())
+    max_date_day = datetime.datetime.strptime(max_date, '%Y/%m/%d').date()
+
+    min_date = '{}/{}'.format(this_year, date_lists[0].strip())
+    min_date_day = datetime.datetime.strptime(min_date, '%Y/%m/%d').date()
+
+    # get diff of day:
+    diff_day = (max_date_day - min_date_day).days
+    print('diff_day: ', diff_day)
+
+    # check cross year issue: (In ALLPOST page, today must >= those dates.)
+    if today_date < min_date_day:
+        print('\ntoday_date - min_date_day < 0:')
+        if diff_day >= 0: # min == max OR min < max ~~> both cross one year.(ex: min 2017/12/30, max 2017/12/31)
+            print('max:', max_date_day, ' == min: ', min_date_day)
+            max_date_day = max_date_day.replace(year = max_date_day.year - 1)
+            min_date_day = min_date_day.replace(year = min_date_day.year - 1)
+            diff_day = (max_date_day - min_date_day).days
+        elif diff_day < 0: # if min > max ~~> only min cross one year.
+            min_date_day = min_date_day.replace(year=min_date_day.year - 1)
+            diff_day = (max_date_day - min_date_day).days
+        else:
+            print('Something wrong. Please check function: get_max_min_date() !')
+
+        print('max:', max_date_day, ', min: ', min_date_day, 'new diff: ', diff_day)
+    else:
+        print('\ntoday_date > min_date_day.')
+
+    print('\nmin_date_day = ', min_date_day)
+    print('\nmax_date_day = ', max_date_day)
+    print('\ndiff_day = ', diff_day)
+    max_min_date = [max_date_day, min_date_day]
+    return max_min_date
+
+
+# check_input_date_range:
+def check_input_date_range(inputDate, minDate, maxDate):
+    try:
+        if inputDate > maxDate:
+            print(colored("""
+                    Input date out of range! \n
+                    Input date must <= {} !""".format(maxDate), 'red'))
+            sys.exit(2)
+        elif inputDate < minDate:
+            print(colored("""
+                    Input date out of range! \n
+                    Input date must >= {} !""".format(minDate), 'red'))
+            sys.exit(2)
+        else:
+            print('Input is OK!')
+    except SystemExit:
+        print('Please try again.')
+        raise
+    return 0
+
+def check_page_range(pageNum):
+    guess_url = 'https://www.ptt.cc/bbs/ALLPOST/index' + str(pageNum).strip() + '.html'
+    print('first url = ', guess_url)
+    guess_soup = get_contentSoup(guess_url)
+    guess_date_range = get_max_min_date(guess_soup)
+    return guess_date_range
+
+def comfirm_page_range(guest_date, input_date_f):
+    if guest_date[0] == input_date_f and guest_date[1] == input_date_f:
+        print('equal!')
+        comfirm = False
+    elif guest_date[0] > input_date_f and guest_date[1] == input_date_f:
+        print('find the end page!')
+        comfirm = True
+    elif guest_date[0] > input_date_f and guest_date[1] > input_date_f:
+        print('check for end page!') # if sep by same page ?! => MAYBE... +/- 1 also equal
+        comfirm = False
+    elif guest_date[0] == input_date_f and guest_date[1] < input_date_f:
+        print('find the start page!')
+        comfirm = True
+    elif guest_date[0] < input_date_f and guest_date[1] < input_date_f:
+        print('check for start page!') # if sep by same page ?! => MAYBE... +/- 1 also equal
+        comfirm = False
+    else:
+        print('guest_1_date[0](max day) = ', guest_date[0])
+        print('guest_1_date[1](min day) = ', guest_date[1])
+        print('input_date_f = ', input_date_f)
+        comfirm = False
+    return comfirm
+
+# use input date to get page range:
+def get_page_range(input_date_f, min_date, max_date, maxPages):
+
+    # count days:
+    diff_day = (max_date - min_date).days + 1
+    print('diff_day: ', diff_day)
+
+    # guess pages:
+    page_list = list(range(1, maxPages, maxPages // diff_day))
+    print('page_list: ', page_list)
+
+    # get list_place:
+    list_place = (input_date_f - min_date).days + 1
+    print('list_place: ', list_place)
+
+    # start pages:
+    start_num = page_list[list_place]
+    print('start_num: ', start_num)
+
+    # getting start page number:
+    guest_1_date = check_page_range(start_num)
+    print('guest_1_date = ', guest_1_date)
+
+    # if else ...
+    comfirm = comfirm_page_range(guest_1_date, input_date_f)
+    print('comfirm = ', comfirm)
+
+    # the guess quite fine, to get +1 day's page:
+    # end_pageNum = start_num + 500
+    # guest_2_date = check_page_range(end_pageNum)
+
+
+
+
+
+
 
 
 
 def main():
-
+    start_time = datetime.datetime.today()
+    ### For Comment Line:
     try:
         # get comment line input:
         opts, operands = getopt.getopt(sys.argv[1:], 'hd:o:')
@@ -72,23 +195,109 @@ def main():
             python3 ptt_all_post_v2.py -h
         """, 'red'))
         sys.exit(2)
+    except IndexError:  # if user don't input arg.
+        print(colored("""
+        Some arguments miss... Please Check your syntax: 
+            python3 ptt_all_post_v2.py -d < yyyy/mm/dd > -o < /outputFile/path >
+        Or input help comment to see more detail:
+            python3 ptt_all_post_v2.py -h
+        """, 'red'))
+        sys.exit(2)
 
     for o, v in opts:
         if o == '-h':
             print("""
-                    ptt_all_post_v2.py -h -d < yyyy/mm/dd > -o < /outputFile/path > 
-                        -h : get help message.
-                        -d : the day you want to crawler.
-                        -o : the path you want to save the data.
-                    """)
+            ptt_all_post_v2.py -h -d < yyyy/mm/dd > -o < /outputFile/path > 
+                -h : get help message.
+                -d : the day you want to crawler.
+                -o : the path you want to save the data.
+            """)
             sys.exit()
 
-    dateStr = opts[0][1]
-    outputFile = opts[1][1]
-    print('dateStr = ', dateStr)
-    print('outputFile = ', outputFile)
-    maxpages = get_max_pageNumber()
-    print('maxpages = ', maxpages, ', type(maxpages) = ', type(maxpages))
+    # For input datetime & file path:
+    try:
+        # date:
+        input_dateStr = opts[0][1]
+        input_date_f = datetime.datetime.strptime(input_dateStr, '%Y/%m/%d').date()
+
+        today_date = datetime.date.today()
+        print('\ndateStr = ', input_dateStr, '\ntype(dateStr) = ', type(input_dateStr))
+        print('\ndate_f = ', input_date_f, '\ntype(date_f) = ', type(input_date_f))
+        print('\ntoday of date = ', today_date, '\ntype(today_date) = ', type(today_date))
+        print('\ndate_f == today_date ? ', input_date_f == today_date)
+
+
+        # file:
+        outputFile = opts[1][1]
+
+        print('\noutputFile = ', outputFile)
+
+    ### For PTT ALLPOST:
+
+        index_url = 'https://www.ptt.cc/bbs/ALLPOST/index.html'
+        index1_url = 'https://www.ptt.cc/bbs/ALLPOST/index1.html'
+        # index page:
+        index_soup = get_contentSoup(index_url)
+        # last page:
+        index1_soup = get_contentSoup(index1_url)
+        # max pages:
+        maxPages = get_total_pageNumber(index_soup)
+        print('\nmaxPages = ', maxPages, ', type(maxPages) = ', type(maxPages))
+
+        # get max_date:
+        max_date = get_max_min_date(index_soup)[0] # return {max, min}
+        print('\nmax_date = ', max_date, ', type(max_date) = ', type(max_date))
+
+        # get min_date:
+        min_date = get_max_min_date(index1_soup)[1] # return {max, min}
+        print('\nmin_date = ', min_date, ', type(min_date) = ', type(min_date))
+
+        # check input date to date range:
+        check_input_date_range(input_date_f, min_date, max_date)
+
+        # count days:
+        diff_day = (max_date - min_date).days + 1
+        print('diff_day: ', diff_day)
+
+        ####### use input date to guess page range:
+        # # guess pages:
+        # page_list = list(range(1, maxPages, maxPages//diff_day))
+        # print('page_list: ', page_list)
+        #
+        # # get list_place:
+        # list_place = (input_date_f - min_date).days + 1
+        # print('list_place: ', list_place)
+        #
+        # # start pages:
+        # start_num = page_list[list_place]
+        # print('start_num: ', start_num)
+        #
+        # # getting start page number:
+        # guess_1_url = 'https://www.ptt.cc/bbs/ALLPOST/index'+str(start_num).strip()+'.html'
+        # print('first url = ', guess_1_url)
+        # guess_1_soup = get_contentSoup(guess_1_url)
+        # guest_1_date = get_max_min_date(guess_1_soup)
+        # print('guest_1_date = ', guest_1_date)
+        output_page_range = get_page_range(input_date_f, min_date, max_date, maxPages)
+
+
+        # counting time:
+        end_time = datetime.datetime.today()
+        time_cost = (end_time - start_time).total_seconds()
+        print('\nstart time: ', start_time)
+        print('\nend time: ', end_time)
+        print('total cost...', time_cost, ' sec.')
+
+    except ValueError: # handel date string error.
+        print(colored("""
+        ValueError! please check syntax:
+        python3 ptt_all_post_v2.py -d < yyyy/mm/dd > -o < /outputFile/path >
+            -d < yyyy/mm/dd >
+            -o < /outputFile/path >
+        """, 'red'))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
 
 if __name__ == "__main__":
     main()
---
+++ ptt_all_post_v3.py
@@ -0,0 +1,295 @@
+import datetime
+import getopt
+import sys
+from termcolor import colored
+
+### Function:
+
+# 1. get content from url:
+def get_contentSoup(url):
+    import requests
+    import urllib3
+    from bs4 import BeautifulSoup
+    from_str = url.split('/', 3)[3]
+    # For over18 POST:
+    load = {
+        'from': from_str,
+        'yes': 'yes'
+    }
+    # For GET:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0',
+        'Host': 'www.ptt.cc',
+        'Connection': 'keep-alive',
+    }
+
+    rs = requests.session()
+    urllib3.disable_warnings()
+    res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=load)
+    res = rs.get(url, headers=headers)
+    if res.status_code == 200:
+        # get page text
+        content = res.text
+        soup = BeautifulSoup(content, "html.parser")
+        return soup
+    else:
+        print(colored('status_code != 200', 'red'))
+
+# 2. get max pages number from index page:
+def get_total_pageNumber(soup):
+    # get next page :
+    next_page = soup.find_all('a', 'btn wide')[1]['href']
+    pageNum = next_page.split('/', 3)[3].split('.')[0]
+    Num = int(pageNum[5:]) + 1
+    return Num
+
+
+
+# 3. get max_date and min_date: the newest article is at end of list.
+def get_max_min_date(soup):
+    import datetime
+    # get date :
+    rent = soup.find_all('div', 'r-ent')
+    date_lists = []
+    for md in rent:
+        date_lists.append(md.find('div', 'date').string)
+
+    today_date = datetime.date.today()
+    this_year = today_date.year
+
+    # put today.year as year: if max = 2018/01/02, min = 2017/12/30 ~> 2018/12/30, today = 2018/01/05
+    max_date = '{}/{}'.format(this_year, date_lists[-1].strip())
+    max_date_day = datetime.datetime.strptime(max_date, '%Y/%m/%d').date()
+
+    min_date = '{}/{}'.format(this_year, date_lists[0].strip())
+    min_date_day = datetime.datetime.strptime(min_date, '%Y/%m/%d').date()
+
+    # get diff of day:
+    diff_day = (max_date_day - min_date_day).days
+    # print('diff_day: ', diff_day)
+
+    # check cross year issue: (In ALLPOST page, today must >= those dates.)
+    if today_date < min_date_day:
+        print('\ntoday_date - min_date_day < 0:')
+        if diff_day >= 0: # min == max OR min < max ~~> both cross one year.(ex: min 2017/12/30, max 2017/12/31)
+            print('max:', max_date_day, ' == min: ', min_date_day)
+            max_date_day = max_date_day.replace(year = max_date_day.year - 1)
+            min_date_day = min_date_day.replace(year = min_date_day.year - 1)
+            diff_day = (max_date_day - min_date_day).days
+        elif diff_day < 0: # if min > max ~~> only min cross one year.
+            min_date_day = min_date_day.replace(year=min_date_day.year - 1)
+            diff_day = (max_date_day - min_date_day).days
+        else:
+            print('Something wrong. Please check function: get_max_min_date() !')
+
+        print('max:', max_date_day, ', min: ', min_date_day, 'new diff: ', diff_day)
+    # else:
+        # print('\ntoday_date > min_date_day.')
+
+    # print('\nmin_date_day = ', min_date_day)
+    # print('\nmax_date_day = ', max_date_day)
+    # print('\ndiff_day = ', diff_day)
+    max_min_date = [max_date_day, min_date_day]
+    return max_min_date
+
+
+def get_content_data(soup):
+    import time
+    import pandas as pd
+    # 進行解析
+    rent = soup.find_all('div', 'r-ent')
+    # get title & href:
+    title_lists = []
+    board_lists = []
+    href_lists = []
+    for title in rent:
+        if title.find('div', 'title').a != None:
+            title_broad = title.find('div', 'title').a.string
+            # print('type of title_broad : ', type(title_broad))
+            try:
+                board = title_broad.split(r"(")[-1].split(r")")[0]
+                title_name = title_broad.split(r"(")[0].strip()
+                # print('board = ', board)
+                # print('title_name = ', title_name)
+            except AttributeError:
+                board = 'NA'
+                title_name = 'NA'
+                print('Some AttributeError...')
+            except:
+                print('Unknown Error')
+
+            title_lists.append(title_name)
+            board_lists.append(board)
+
+            href_no = title.find('div', 'title').a['href'].split('/')[-1]
+            href = '/bbs/' + board + '/' + href_no
+            href_lists.append(href)
+
+        else:   # all have title even if it doesn't exist
+            print('Something wrong...')
+    # get date :
+    date_lists = []
+    for md in rent:
+        date_lists.append(md.find('div', 'date').string)
+    # get author :
+    author_lists = []
+    for author in rent:
+        author_lists.append(author.find('div', 'author').string)
+    # get info time :
+    timenow = time.localtime()
+    get_time = time.strftime("%Y-%m-%d %H:%M:%S", timenow)
+    # get r-ent info :
+    r_ent_df = pd.DataFrame({'board': board_lists,
+                             'title': title_lists,
+                             'href': href_lists,
+                             'dates': date_lists,
+                             'author': author_lists,
+                             'get_time': get_time})
+    return r_ent_df
+
+
+
+
+
+def crawler_allpost(maxPages, target_days):
+    import pandas as pd
+    df = pd.DataFrame()
+    for pages in range(maxPages, 0, -1):
+        page_url = 'https://www.ptt.cc/bbs/ALLPOST/index'+str(pages).strip()+'.html'
+        page_soup = get_contentSoup(page_url)
+        page_max_min_date = get_max_min_date(page_soup)  # return {max, min} = { [0] , [1] }
+        if page_max_min_date[1] >= target_days:
+            # print('Processing Page ' + str(pages) + ' ...')
+            r_ent_df = get_content_data(page_soup)
+            df = df.append(r_ent_df, ignore_index=True)
+        elif page_max_min_date[1] < target_days and page_max_min_date[0] >= target_days:
+            # print('Processing Page ' + str(pages) + ' ...')
+            r_ent_df = get_content_data(page_soup)
+            df = df.append(r_ent_df, ignore_index=True)
+        else:
+            # print('Finish!')
+            break
+    return df
+
+
+
+##### main():
+def main():
+    start_time = datetime.datetime.today()
+    ### get arg from Comment Line:
+    try:
+        # get comment line input:
+        opts, operands = getopt.getopt(sys.argv[1:], 'hd:o:')
+    except getopt.GetoptError:
+        print(colored("""
+        Something wrong... Please Check your syntax:
+            python3 ptt_all_post_v3.py -d < how many days:int > -o < /outputFile/path >
+        Or input help comment to see more detail:
+            python3 ptt_all_post_v3.py -h
+        """, 'red'))
+        sys.exit(2)
+    except IndexError:  # if user don't input arg.
+        print(colored("""
+        Some arguments miss... Please Check your syntax: 
+            python3 ptt_all_post_v3.py -d < how many days:int > -o < /outputFile/path >
+        Or input help comment to see more detail:
+            python3 ptt_all_post_v3.py -h
+        """, 'red'))
+        sys.exit(2)
+
+    for o, v in opts:
+        if o == '-h':
+            print(colored("""
+            python3 ptt_all_post_v3.py -d < how many days:int > -o < /outputFile/path >
+                -h : get help message.
+                -d : How many day you want to crawler? (input an integer)
+                        The range is from 1 to 7. (PTT ALLPOST just save 7~8 days)
+                        1 for today until now, 2 for yesterday until now...ect.
+                -o : output file path.
+            """, "blue"))
+            sys.exit()
+
+    # For input datetime & file path:
+    try:
+        # date:
+        int_days = opts[0][1]
+
+        # if type(int_days) != 'int':
+        #     print(colored("Input value must be integer !", 'red'))
+        #     sys.exit()
+
+        int_days = int(int_days)
+
+        if int_days < 1:
+            print(colored("Input value can't smaller than 1 !", 'red'))
+            sys.exit()
+        elif int_days > 7:
+            print(colored("Input value can't larger than 7 !", 'red'))
+            sys.exit()
+
+        today_date = datetime.date.today()
+        # Find out target day:
+        target_days = today_date - datetime.timedelta(days = int_days - 1)
+        print('target_days = ', target_days)
+        print('\ndate_f == today_date ? ', target_days == today_date)
+
+        # file:
+        outputFile = opts[1][1]
+
+        print('\noutputFile = ', outputFile)
+
+    ### For PTT ALLPOST: check
+
+        # index page: (The newest page)
+        index_url = 'https://www.ptt.cc/bbs/ALLPOST/index.html'
+        index_soup = get_contentSoup(index_url)
+
+        # last page: (The oldest page)
+        index1_url = 'https://www.ptt.cc/bbs/ALLPOST/index1.html'
+        index1_soup = get_contentSoup(index1_url)
+
+        # max pages:
+        maxPages = get_total_pageNumber(index_soup)
+        print('\nmaxPages = ', maxPages)
+
+        # get max_date:
+        max_date = get_max_min_date(index_soup)[0] # return {max, min}
+        print('\nmax_date = ', max_date)
+
+        # get min_date:
+        min_date = get_max_min_date(index1_soup)[1] # return {max, min}
+        print('\nmin_date = ', min_date)
+
+        # START crawler: -> loop from maxPages and check date every page.
+        result_df = crawler_allpost(maxPages, target_days)
+        # print('\nresult_df = ', result_df)
+        try:
+            result_df.to_csv(outputFile, sep='|', encoding='utf-8')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+        # counting time:
+        end_time = datetime.datetime.today()
+        time_cost = (end_time - start_time).total_seconds()
+        print('\nstart time: ', start_time)
+        print('\nend time: ', end_time)
+        print('total cost...', time_cost, ' sec.')
+
+    except ValueError: # handel date string error.
+        print(colored("""
+        ValueError! please check syntax:
+        python3 ptt_all_post_v3.py -d < how many days:int > -o < /outputFile/path >
+        
+            -d how many days want to get? (input an integer)
+            -o /.../outputFile/path  
+        """, 'red'),
+              '\n', colored("\tDetail information:", 'red'),
+              '\n\t', colored(sys.exc_info()[1], 'red'))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+
+if __name__ == "__main__":
+    main()
